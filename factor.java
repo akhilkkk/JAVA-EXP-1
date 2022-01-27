408e6aba00daa2ce1b4db48d189d3cbef58b12c1
AKHIL-KRISHNAN <98377774+akhilkkk@users.noreply.github.com>
2022-01-27 05:04:30 +0000

    Update factor.java

--- factor.java
+++ factor.java
@@ -4,14 +4,14 @@ class factor
  public static void main(String args[])
  {
   int a,i,b;
-  System.out.println("\n Enter the number: ");
+  System.out.println(" Enter the number: ");
   Scanner obj=new Scanner(System.in);
   a=obj.nextInt();
   for(i=1;i<=a;i++)
   {
    b=a%i;
    if(b==0)
-    System.out.println(+i);
+    System.out.println("Factors are: "+i);
   }
  }
 }
